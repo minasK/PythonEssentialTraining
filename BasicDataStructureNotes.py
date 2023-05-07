38507cf0313f04e488fdc4d59d9fc5748b99ba80
minasK <M.Kasmas@acg.edu>
2023-05-07 21:17:04 +0300

    BasicDataStructure

---
+++ BasicDataStructureNotes.py
@@ -0,0 +1,15 @@
+myList = [1,2,3,4,5]
+print(myList[3:])
+print(myList[0:6:2])
+print(myList[::2])
+
+for i in range (100):
+    print(i)
+
+myList = list(range(100))
+print(myList[::6])
+print(myList[::-6])
+
+myList = [1,2,3,4]
+myList.append(6)
+print(myList)
